8960e1c0a775b44b979dc2f1b4dac64972bda18a
weihu <weihu@jd.com>
2019-02-19 22:09:39 +0800

    redis连接池

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+import redis
+#
+# conn=redis.Redis(host='192.168.25.132',port=6379)
+# # conn.set('x1','huge',ex=5)
+#
+# print(conn.get('x1'))
 
-# Create your tests here.
+pool=redis.ConnectionPool(host='192.168.25.132',port=6379,max_connections=1000)
+r=redis.Redis(connection_pool=pool)
+# r.set('foo','Bar')
+print(r.get('foo'))
